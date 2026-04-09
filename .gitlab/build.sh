181406c414f579c4123ab03a8d788999180a1331
Iaroslav Sydoruk <isydoruk@nvidia.com>
2026-04-09 14:00:12 +0300

    Fixed python tests
    partial_md_example.py:119 unconditionally creates agents with enable_listen_thread=True
    and hardcoded default ports (5555 for target, 7777 for initiator),
    regardless of whether --etcd or socket mode is used
    
    ```
    [2026-03-02T17:34:37.516Z] + python3 partial_md_example.py --etcd
    [2026-03-02T17:34:38.149Z] 2026-03-02 09:34:37 NIXL INFO    partial_md_example.py:94 Using NIXL Plugins from: /opt/nixl/lib/aarch64-linux-gnu/plugins
    [2026-03-02T17:34:38.149Z] 2026-03-02 09:34:37 NIXL INFO    partial_md_example.py:102 NIXL_ETCD_ENDPOINTS is set, using endpoints: http://127.0.0.1:59501
    [2026-03-02T17:34:38.149Z] E0302 09:34:37.704217  464337 metadata_stream.cpp:84] Socket Bind failed while setting up listener for MD: Address already in use [98]
    ```
    
    Signed-off-by: Iaroslav Sydoruk <isydoruk@nvidia.com>

--- .gitlab/build.sh
+++ .gitlab/build.sh
@@ -38,7 +38,7 @@ GRPC_TAG=${GRPC_TAG:-v1.73.0}
 # LIBFABRIC_INSTALL_DIR can be set via environment variable, defaults to INSTALL_DIR
 LIBFABRIC_INSTALL_DIR=${LIBFABRIC_INSTALL_DIR:-$INSTALL_DIR}
 # UCCL_COMMIT_SHA is the commit SHA of UCCL.
-UCCL_COMMIT_SHA="2de728f1a27ea3f3b66059baf838f940e243ebc6"
+UCCL_COMMIT_SHA="4ca420138aa7161c6bfd76ebda51ba4c07173c6d"
 AZURITE_VER="3.35.0"
 TMPDIR=$(mktemp -d)
 
@@ -137,7 +137,8 @@ else
         click tabulate auditwheel tomlkit \
         pytest pytest-timeout zmq \
         mpmath typing-extensions sympy numpy \
-        networkx MarkupSafe fsspec filelock jinja2 torch
+        networkx MarkupSafe fsspec filelock jinja2 torch \
+        nanobind
 
     # Add DOCA repository and install packages
     ARCH_SUFFIX=$(if [ "${ARCH}" = "aarch64" ]; then echo "arm64"; else echo "amd64"; fi)
